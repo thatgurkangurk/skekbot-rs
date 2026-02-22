c269aaa04d8c8ef8b4e9c00695944c6e79b9317a
Gurkan <hello@gurkz.me>
2026-02-22 17:51:24 +0100

    ping command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+mod commands;
+
 use std::env;
 
 use serenity::async_trait;
+use serenity::builder::{CreateInteractionResponse, CreateInteractionResponseMessage};
+use serenity::model::application::{Command, Interaction};
 use serenity::model::gateway::Ready;
 use serenity::prelude::*;
 
@@ -8,8 +12,31 @@ struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn ready(&self, _: Context, ready: Ready) {
+    async fn ready(&self, ctx: Context, ready: Ready) {
         println!("Logged in as {}", ready.user.tag());
+
+        let _ = Command::create_global_command(&ctx.http, commands::ping::register()).await;
+
+        // println!("i created the following global slash command: {global_command:#?}");
+    }
+
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::Command(command) = interaction {
+            // println!("received command interaction: {command:#?}");
+
+            let content = match command.data.name.as_str() {
+                "ping" => Some(commands::ping::run(&command.data.options())),
+                _ => Some("not implemented :( (or i just forgot to register it)".to_string()),
+            };
+
+            if let Some(content) = content {
+                let data = CreateInteractionResponseMessage::new().content(content);
+                let builder = CreateInteractionResponse::Message(data);
+                if let Err(why) = command.create_response(&ctx.http, builder).await {
+                    println!("cannot respond to slash command: {why}");
+                }
+            }
+        }
     }
 }
 
@@ -19,10 +46,9 @@ async fn main() {
 
     let token = env::var("DISCORD_TOKEN")
         .expect("expected the DISCORD_TOKEN environment variable to exist");
-    
-    let intents = GatewayIntents::GUILDS
-        | GatewayIntents::GUILD_MESSAGES
-        | GatewayIntents::MESSAGE_CONTENT;
+
+    let intents =
+        GatewayIntents::GUILDS | GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
 
     let mut client = Client::builder(&token, intents)
         .event_handler(Handler)
