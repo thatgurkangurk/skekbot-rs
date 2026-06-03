3d2b64fa0e10fe2a89d588d7adc8a1ea94a9bbab
Skekdog <47750815+Skekdog@users.noreply.github.com>
2026-06-03 20:32:22 +0100

    Add 8 ball responses
    My source is wikipedia

--- src/commands/fun.rs
+++ src/commands/fun.rs
@@ -5,10 +5,29 @@ use tokio::time::sleep;
 use crate::{Context, Data, Error};
 
 const EIGHT_BALL_ANSWERS: &[&str] = &[
-    "test",
-    "i forgot what the answers were",
-    "test 2",
-    "ill fix this later",
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes definitely",
+    "You may rely on it",
+
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    
+    "Reply hazy, try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful",
 ];
 
 #[poise::command(slash_command, rename = "8ball")]
